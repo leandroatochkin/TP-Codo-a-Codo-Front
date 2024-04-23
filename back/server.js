71cba7b685e122b2bdd84717e9191e3672772475
leandroatochkin <126533014+leandroatochkin@users.noreply.github.com>
2024-04-22 21:22:29 -0300

    Update server.js

--- back/server.js
+++ back/server.js
@@ -11,7 +11,7 @@ const port = 3000;
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'LeandrO1990!!',
+  password: '*****',
   database: 'book_store'
 });
 
